c2ea334d9be67625f52a20deb2ad21fe0b4d2393
Layanne Alves <94229957+eiLayanneAlves@users.noreply.github.com>
2026-02-24 14:22:04 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+// Lista de professores
+const professores = [
+  { cpf: "12345678900", nascimento: "1980-05-12", nome: "Prof. João" },
+  { cpf: "98765432100", nascimento: "1975-11-30", nome: "Prof. Maria" }
+];
+
+// Lista de alunos
+const alunos = [
+  { id: 1, nome: "Alice", faltas: 0, deveresNaoFeitos: 0 },
+  { id: 2, nome: "Bruno", faltas: 0, deveresNaoFeitos: 0 },
+  { id: 3, nome: "Carla", faltas: 0, deveresNaoFeitos: 0 }
+];
